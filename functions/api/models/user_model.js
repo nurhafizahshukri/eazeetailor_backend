74092c5bade0f49a989d88de9891979e5344775a
Nurhafizah Shukri <nfizaaaah@gmail.com>
2020-07-25 17:35:30 +0800

    define model for user

---
+++ functions/api/models/user_model.js
@@ -0,0 +1,31 @@
+const database = require("../database");
+
+
+class UserModel {
+  constructor() {
+    if (this.instance) return this.instance;
+    UserModel.instance = this;
+  }
+
+  get() {
+    return database.getList("user");
+  }
+  getById(id) {
+    return database.get("user", id);
+  }
+
+  create(user) {
+    return database.create("user", user);
+  }
+
+  delete(id) {
+    return database.delete("user", id);
+  }
+
+  update(id, user) {
+    return database.set("user", id, user);
+  }
+
+}
+
+module.exports = new UserModel();
