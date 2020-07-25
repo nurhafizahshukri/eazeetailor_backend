1d531a01cbb7c39f3845f6034f5db89984328bc6
Husna Azmi <husna.azmi98@gmail.com>
2020-07-25 19:59:17 +0800

    define model for appointment

---
+++ functions/api/models/appointment_model.js
@@ -0,0 +1,32 @@
+const database = require("../database");
+
+// Here, we are implementing the class with Singleton design pattern
+
+class ApptsModel {
+  constructor() {
+    if (this.instance) return this.instance;
+    ApptsMode.instance = this;
+  }
+
+  get() {
+    return database.getList("appointment");
+  }
+
+  getById(id) {
+    return database.get("appointment", id);
+  }
+
+  create(appt) {
+    return database.create("appointment", appt);
+  }
+
+  delete(id) {
+    return database.delete("appointment", id);
+  }
+
+  update(id, appt) {
+    return database.set("appointment", id, appt);
+  }
+}
+
+module.exports = new ApptsModel();
