6ec5896bec55126d304fb5cc66d3d0413ee9299c
farra <ainfarra11@gmail.com>
2020-07-26 12:57:17 +0800

    add router for order

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,14 @@ const express = require("express");
 const app = express();
 const membersRouter = require("./api/controllers/membersize_controller");
 const userRouter = require("./api/controllers/user_controller");
-const appointmentRouter = require("./controllers/appointment_controller");
+const appointmentRouter = require("./api/controllers/appointment_controller");
+const orderRouter = require("./api/controllers/order_controller");
 
 app.use(express.json());
 app.use("/members", membersRouter);
 app.use("/user", userRouter);
 app.use("/appointment", appointmentRouter);
+app.use("/order", orderRouter);
 
 exports.api = functions.https.onRequest(app);
 
